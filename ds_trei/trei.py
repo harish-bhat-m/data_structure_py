2321ac4273fc6664e484960e9e8de8858593a744
Harish Bhat M <harish.bhat.m@gmail.com>
2019-09-26 22:50:23 +0530

    Initial commit for trei data structure

---
+++ ds_trei/trei.py
@@ -0,0 +1,42 @@
+class TreiNode(object):
+    def __init__(self):
+        self.children = [None] * 26
+        self.last = False
+
+
+class Trei(object):
+
+    def __init__(self):
+        self.root  = TreiNode()
+
+    def _charToIndex(self, char):
+        return ord(char) - ord('a')
+    
+    def insert(self, word):
+        node = self.root
+        #print(word)
+        #wordLen = len(word)
+        for char in word:
+            #print(char)
+            index = self._charToIndex(char)
+            if not node.children[index]:
+                node.children[index] = TreiNode()
+
+            node = node.children[index]
+        node.last = True
+
+    def iterate(self):
+        for i in self.root:
+            print(i)
+
+def main():
+    trei = Trei()
+    words = ["hello", "how", "are", "you"]
+    for word in words:
+        trei.insert(word)
+    
+    trei.iterate()
+    
+        
+if __name__ == "__main__":
+    main()
